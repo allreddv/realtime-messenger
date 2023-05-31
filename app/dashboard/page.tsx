c52859f89f6433da08b55bd2283e4fb6c7a88987
allreddv <allreddv@gmail.com>
2023-05-30 18:23:15 -0700

    adding friends

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import Button from '@/components/ui/Button';
-import { Ghost } from 'lucide-react';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+
 import { FC } from 'react';
 
-interface pageProps {}
-
-const page: FC<pageProps> = ({}) => {
-  return <Button>hello</Button>;
+const page = async ({}) => {
+  const session = await getServerSession(authOptions);
+  return <pre>{JSON.stringify(session)}</pre>;
 };
 
 export default page;
